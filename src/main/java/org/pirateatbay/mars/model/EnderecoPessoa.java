156e76d05cea2f01df296aa538c4d24e13cd0abb
Josiel M. Silva <josiel1nico@gmail.com>
2015-06-03 16:16:47 -0300

    Criando model EnderecoPessoa

---
+++ src/main/java/org/pirateatbay/mars/model/EnderecoPessoa.java
@@ -0,0 +1,100 @@
+package org.pirateatbay.mars.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class EnderecoPessoa {
+
+	@Id
+	@GeneratedValue
+	private long idEnderecoPessoa;
+
+	@ManyToOne
+	@JoinColumn(name = "id_pessoa")
+	private Pessoa pessoa;
+
+	private String rua;
+	private int numero;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	private String cep;
+
+	public long getIdEnderecoPessoa() {
+		return idEnderecoPessoa;
+	}
+
+	public void setIdEnderecoPessoa(long idEnderecoPessoa) {
+		this.idEnderecoPessoa = idEnderecoPessoa;
+	}
+
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+}
