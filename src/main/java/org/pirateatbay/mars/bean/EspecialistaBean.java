64646c70149e5c88248f7cf5d1ef66bfe67769b7
Pablo Gabriel <pablo.reds@hotmail.com>
2015-06-08 03:16:32 -0300

    #3: Creating alterar_especialista.xhtml and remover_especialista.xhtml

--- src/main/java/org/pirateatbay/mars/bean/EspecialistaBean.java
+++ src/main/java/org/pirateatbay/mars/bean/EspecialistaBean.java
@@ -1,12 +1,10 @@
 package org.pirateatbay.mars.bean;
 
-import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import javax.faces.context.FacesContext;
 
 import org.pirateatbay.mars.dao.EspecialistaDao;
 import org.pirateatbay.mars.model.Especialista;
@@ -46,49 +44,32 @@ public class EspecialistaBean implements Serializable {
 	
 	public void createEspecialista() {
 		especialistaDao.save(especialista);
-		success();
 	}
 	
 	public void updateEspecialista() {
 		Especialista e = especialistaDao.find(especialista.getIdEspecialista());
 		especialistaDao.update(e);
+		especialistas = null; // Update list 'especialistas'
 	}
 	
 	public void deleteEspecialista() {
 		Especialista e = especialistaDao.find(especialista.getIdEspecialista());
-		System.out.println(especialista.getIdEspecialista());
 		especialistaDao.delete(e);
+		especialistas = null; // Update list 'especialistas'
 	}
 	
-	public void clear() {
-		especialista = new Especialista();
+	public String cancel() {
+		return "index.xhtml";
 	}
 	
-	public void cancel() throws IOException {
-		clear();
-		String uri = "index.xhtml";
-	    FacesContext.getCurrentInstance().getExternalContext().dispatch(uri);
+	public String success() {
+		return "index.xhtml?faces-redirect=true";
 	}
 	
-	public void success() {
-		clear();
-	    try {
-			String uri = "index.xhtml";
-			FacesContext.getCurrentInstance().getExternalContext().dispatch(uri);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public void backward() {
 	}
 	
-	public void backward() throws IOException {
-		clear();
-		String uri = "cadastrar_especialista.xhtml";
-	    FacesContext.getCurrentInstance().getExternalContext().dispatch(uri);
-	}
-	
-	public void forward() throws IOException {
-	    String uri = "cadastrar_endereco.xhtml";
-	    FacesContext.getCurrentInstance().getExternalContext().dispatch(uri);
+	public void forward() {
 	}
 
 }
